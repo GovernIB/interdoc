8fcc1223a9b9ff5e944a739829a16f862bc7e982
fbosch-fundaciobit <fbosch@fundaciobit.org>
2025-08-05 10:09:08 +0200

    Obrir Versió #14

---
+++ scripts/bbdd/oracle/backup_data_dev.sql
@@ -0,0 +1,180 @@
+INSERT INTO INTERDOC.ITD_APLICACIO (APLICACIOID,USUARI,CLAU,NOM,CODIDIR3,DATACREACIO,ESTAT) VALUES
+	 (2,'fbit','interdoc','Usuari Aplicació Fundació Bit','A04019281',TIMESTAMP'2023-11-07 12:13:44.970275',1),
+	 (1,'caib','interdoc','Usuari Aplicació Caib','A04019899',TIMESTAMP'2023-11-07 12:14:12.744536',1);
+INSERT INTO INTERDOC.ITD_ENTITAT (ENTITATID,NOM,CODIDIR3,DATACREACIO,ACTIU) VALUES
+	 (1,'CAIB','A04003003',TIMESTAMP'2023-11-07 12:13:16.945555',1),
+	 (1000,'FBIT','A04027052',TIMESTAMP'2024-03-11 00:00:00',1);
+INSERT INTO INTERDOC.ITD_FITXER (FITXERID,NOM,DESCRIPCIO,MIME,TAMANY,DATACREACIO,RUTA) VALUES
+	 (1040,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_130527335.pdf'),
+	 (1042,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_131628777.pdf'),
+	 (1043,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_133643978.pdf'),
+	 (1180,'Carta DG de Salut Pu_blica.pdf','Carta DG de Salut Pu_blica.pdf','application/pdf',364875,TIMESTAMP'2024-01-09 00:00:00','C:\Dades\Programacio\interdoc-files\20240109_110003270.pdf'),
+	 (1000,'fitxer','fitxer','application/pdf',519313,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1699976097544_fitxer3337580431128223892.'),
+	 (1020,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_092910064.pdf'),
+	 (1044,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_135327205.pdf'),
+	 (1061,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_150740609.pdf'),
+	 (1080,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-16 00:00:00','C:\Dades\Programacio\interdoc-files\20231116_092745671.pdf'),
+	 (1081,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-16 00:00:00','C:\Dades\Programacio\interdoc-files\20231116_093430960.pdf');
+INSERT INTO INTERDOC.ITD_FITXER (FITXERID,NOM,DESCRIPCIO,MIME,TAMANY,DATACREACIO,RUTA) VALUES
+	 (1082,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-16 00:00:00','C:\Dades\Programacio\interdoc-files\20231116_093935891.pdf'),
+	 (1100,'FICHERO.PDF','FICHERO DESC','APPLICATION/PDF',111400,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1700469078720_FICHERO.PDF11351814138437662138.PDF'),
+	 (1120,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-20 00:00:00','C:\Dades\Programacio\interdoc-files\20231120_224347690.pdf'),
+	 (1141,'Mi_Fichero','fichero','application/pdf',7944,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1700563138138_Mi_Fichero8753085870490811930.'),
+	 (1142,'Mi_Fichero','fichero','application/pdf',7944,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1700563999073_Mi_Fichero1662099051577442688.'),
+	 (1220,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_152013837.pdf'),
+	 (1221,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1705587629506_Circular Rueta 2024.pdf6872125255346464581.pdf'),
+	 (1160,'interdocfile','interdocfile.pdf','application/pdf',7944,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1702383236864_interdocfile8073202445085513321.'),
+	 (1201,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_100006071.pdf'),
+	 (1060,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-15 00:00:00','C:\Dades\Programacio\interdoc-files\20231115_143105419.pdf');
+INSERT INTO INTERDOC.ITD_FITXER (FITXERID,NOM,DESCRIPCIO,MIME,TAMANY,DATACREACIO,RUTA) VALUES
+	 (1083,'interdocfile.pdf','interdocfile.pdf','application/pdf',7944,TIMESTAMP'2023-11-16 00:00:00','C:\Dades\Programacio\interdoc-files\20231116_093936112.pdf'),
+	 (1140,'Mi_Fichero','fichero','application/pdf',7944,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1700562778360_Mi_Fichero13280969787050968781.'),
+	 (1143,'Mi_Fichero','fichero','application/pdf',7944,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1700565860127_Mi_Fichero12212766591775362589.'),
+	 (1182,'Carta DG de Salut Pu_blica.pdf','Carta DG de Salut Pu_blica.pdf','application/pdf',364875,TIMESTAMP'2024-01-09 00:00:00','C:\Dades\Programacio\interdoc-files\20240109_110226737.pdf'),
+	 (1183,'Carta DG de Salut Pu_blica.pdf','Carta DG de Salut Pu_blica.pdf','application/pdf',364875,TIMESTAMP'2024-01-09 00:00:00','C:\Dades\Programacio\interdoc-files\20240109_110852664.pdf'),
+	 (1202,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_110130043.pdf'),
+	 (1203,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_111150464.pdf'),
+	 (1204,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_112938570.pdf'),
+	 (1205,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_113545348.pdf'),
+	 (1206,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1705574166010_Circular Rueta 2024.pdf2049893326232655597.pdf');
+INSERT INTO INTERDOC.ITD_FITXER (FITXERID,NOM,DESCRIPCIO,MIME,TAMANY,DATACREACIO,RUTA) VALUES
+	 (1207,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,TIMESTAMP'2024-01-18 00:00:00','C:\Dades\Programacio\interdoc-files\20240118_120511081.pdf'),
+	 (1208,'Circular Rueta 2024.pdf','Circular Rueta 2024.pdf','application/pdf',184577,NULL,'C:\Users\jagarcia\AppData\Local\Temp\1705575930862_Circular Rueta 2024.pdf15554747140647137428.pdf');
+INSERT INTO INTERDOC.ITD_INFOARXIU (INFOARXIUID,ORIGINALFILEURL,CSV,CSVGENERATIONDEFINITION,CSVVALIDATIONWEB,ARXIUEXPEDIENTID,ARXIUDOCUMENTID,PRINTABLEURL,ENIFILEURL,VALIDATIONFILEURL,ESTATEXPEDIENT,REINTENTS) VALUES
+	 (1060,NULL,'5b06bc6361c70d5c199c752136d7277e04a1ebe1cbac889e5a1925736ce6fe35',NULL,NULL,'8fa6dcc1-78b9-45f3-a700-dd5282cb7661','a5a32e4d-9304-465f-9e32-008987a98a28',NULL,NULL,NULL,NULL,NULL),
+	 (1020,NULL,'1d8f036fa7c0e0bf1fe4fdc2a27bf0338729b2f7222e83a1486e3c203768ff67',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1000,NULL,'8e4d56f0d4f848d192d03bcc891b197bd16e73f8e825693338c8a7ff68dab846',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1040,NULL,'24e00cc00af5b76bf5e2b7b7d2c063a3ba0bb00ac53417b570eaacaa5413ece0',NULL,NULL,'545c13db-b794-4291-94db-0275c8578382','eda7c23b-0690-4257-ac10-f7436c7aa0b6',NULL,NULL,NULL,'O',10),
+	 (1041,NULL,'2df46dc9b391cceb294e0ab051751a67c039a8989b6b80e86670a09b61c65e73',NULL,NULL,'a07081f9-f97d-409e-8bc8-7bd92ac71446','4c527b56-0d1c-47e4-b502-0ab38c147033',NULL,NULL,NULL,'O',10);
+INSERT INTO INTERDOC.ITD_INFOSIGNATURA (INFOSIGNATURAID,SIGNOPERATION,SIGNTYPE,SIGNALGORITHM,SIGNMODE,SIGNATURESTABLELOCATION,TIMESTAMPINCLUDED,POLICYINCLUDED,ENITIPOFIRMA,ENIPERFILFIRMA,ENIROLFIRMA,ENISIGNERNAME,ENISIGNERADMINISTRATIONID,ENISIGNLEVEL,CHECKADMINISTRATIONIDOFSIGNER,CHECKDOCUMENTMODIFICATIONS,CHECKVALIDATIONSIGNATURE,SIGNDATE,"signId",FILENAME,FILEMIME) VALUES
+	 (1000,0,'PAdES','SHA-1',0,0,'0','0','TF06',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1021,0,'PAdES','SHA-1',0,0,'0','0','TF06',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1080,0,'PAdES','SHA-1',0,0,'0','0','TF06','AdES-BES',NULL,NULL,NULL,NULL,NULL,'1','1',TIMESTAMP'2024-01-18 15:20:32.989',NULL,NULL,NULL),
+	 (1040,0,'PAdES','SHA-1',0,0,'0','0','TF06',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1020,0,'PAdES','SHA-1',0,0,'0','0','TF06',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1022,0,'PAdES','SHA-1',0,0,'0','0','TF06',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	 (1060,0,'PAdES','SHA-1',0,0,'0','0','TF06','AdES-BES',NULL,NULL,NULL,NULL,NULL,'1','1',TIMESTAMP'2024-01-18 11:36:10.685',NULL,NULL,NULL),
+	 (1061,0,'PAdES','SHA-1',0,0,'0','0','TF06','AdES-BES',NULL,NULL,NULL,NULL,NULL,'1','1',TIMESTAMP'2024-01-18 12:05:34.075',NULL,NULL,NULL);
+INSERT INTO INTERDOC.ITD_PLUGIN (PLUGINID,NOM,PROPIETATS,CLASSE,DATACREACIO,ACTIU,ENTITATID,TIPUS) VALUES
+	 (1,'Arxiu',TO_CLOB('# Properties arxiu
+es.caib.interdoc.plugins.arxiu.serieDocumental=S0001
+es.caib.interdoc.plugins.arxiu.classificacio=organo1_PRO_123456789
+es.caib.interdoc.plugins.arxiu.aplicacio=Tests
+es.caib.interdoc.plugins.arxiu.tancarExpedient=true
+es.caib.interdoc.plugins.arxiu.csv.validation.url=https://proves.caib.es/concsv/view.xhtml?hash=
+es.caib.interdoc.plugins.arxiu.csv.url=https://proves.caib.es/concsv/rest/printable/uuid/'),'es.caib.interdoc.plugins.arxiu.ArxiuPluginImpl',TIMESTAMP'2023-11-07 12:14:33.347866 Europe/Paris',1,1,1),
+	 (2,'Firma',TO_CLOB('# Properties Firma Simple
+es.caib.interdoc.plugins.firma.profilecades=PROFILE_CADES
+es.caib.interdoc.plugins.firma.profilepades=ENVIAFIB_PADES
+es.caib.interdoc.plugins.firma.profilexades=PROFILE_XADES
+es.caib.interdoc.plugins.firma.administracionId=
+es.caib.interdoc.plugins.firma.nombre=
+es.caib.interdoc.plugins.firma.email=
+es.caib.interdoc.plugins.firma.languageUI=ca
+es.caib.interdoc.plugins.firma.languageSign=ca
+es.caib.interdoc.plugins.firma.localizacion=Palma
+es.caib.interdoc.plugins.firma.motivo=Test
+es.caib.interdoc.plugins.firma.tipodocumentalid=99
+es.caib.interdoc.plugins.firma.signId=1
+es.caib.interdoc.plugins.firma.alias=preprod-dgmad
+# preprod-dgmad afirmades-firma
+es.caib.interdoc.plugins.firma.applicationId=CAIBDEV2.REGWEB
+es.caib.interdoc.plugins.firma.debug=true;
+es.caib.interdoc.plugins.firma.profile=PROFILE_PADES'),'es.caib.interdoc.plugins.apifirmasimple.FirmaPluginImpl',TIMESTAMP'2023-11-07 12:14:48.444537 Europe/Paris',1,1,2);
+INSERT INTO INTERDOC.ITD_REFERENCIA (REFERENCIAID,CSVIDENTIFICADOR,UUID,DIRECCIO,HASH,EMISOR,RECEPTOR,URLVISIBLE,DATACREACIO,REFERENCIA,ENTITATID,INFOSIGNATURAID,INFOARXIUID,FORMATFIRMA,FITXERID,NUMEROREGISTRE) VALUES
+	 (61,'b5ebc4b20afd677efb1c9eff8249d10d',NULL,'http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs?wsdl','a1ddeba635eb8e26cff0ff29d6428c533bbfe57e13be18b04f892e8177c15418','emisor','receptor','http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs',TIMESTAMP'2024-01-18 00:00:00','b5ebc4b20afd677efb1c9eff8249d10d',1,1080,1060,'TF06',1221,NULL),
+	 (21,'csv01101321321za',NULL,'http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWsService/CSVQueryDocumentWs?wsdl','01e5a46e553015cb3daeadff5a49129c73ee4b60abf9b349bc85b08138942b8c','emisor','receptor','http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWsService/CSVQueryDocumentWs',TIMESTAMP'2023-12-12 00:00:00','97b01d809105f6a4f934b3bbf6c51d97',1,1040,1020,'TF06',1160,NULL),
+	 (1,'mixsc',NULL,'http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWsService/CSVQueryDocumentWs?wsdl','01e5a46e553015cb3daeadff5a49129c73ee4b60abf9b349bc85b08138942b8c','emisor','receptor','http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWsService/CSVQueryDocumentWs',TIMESTAMP'2023-11-21 00:00:00','448691d2ada2ebc83dac0a2d49ecabbe',1,1022,1000,'TF06',1143,NULL),
+	 (41,'10aa3eba61536a5db6ebc34ae12a88a0',NULL,'http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs?wsdl','a1ddeba635eb8e26cff0ff29d6428c533bbfe57e13be18b04f892e8177c15418','emisor','receptor','http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs',TIMESTAMP'2024-01-18 00:00:00','10aa3eba61536a5db6ebc34ae12a88a0',1,1060,1040,'TF06',1206,NULL),
+	 (42,'estoeselcsv',NULL,'http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs?wsdl','a1ddeba635eb8e26cff0ff29d6428c533bbfe57e13be18b04f892e8177c15418','emisor','receptor','http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs',TIMESTAMP'2024-01-18 00:00:00','975e997a86c2c4dfa3b40d462628c7b2',1,1061,1041,'TF06',1208,NULL);
+INSERT INTO INTERDOC.ITD_REFERENCIAXML (REFERENCIAXMLID,RESULTAT,DATACREACIO,REFERENCIAID) VALUES
+	 (61,TO_CLOB('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ReferenciaDocumento>
+    <Identificador>
+        <eEMGDE.Firma.FormatoFirma.ValorCSV>csv</eEMGDE.Firma.FormatoFirma.ValorCSV>
+    </Identificador>
+    <Permiso>
+        <Publico>PUBLICO</Publico>
+    </Permiso>
+    <Hash>a1ddeba635eb8e26cff0ff29d6428c533bbfe57e13be18b04f892e8177c15418</Hash>
+    <Direccion>http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs?wsdl</Direccion>
+    <Emisor>
+        <Organismo>emisor</Organismo>
+    </Emisor>
+    <Receptor>
+        <Organismo>receptor</Organismo>
+    </Receptor>
+</ReferenciaDocumento>
+'),TIMESTAMP'2024-01-18 00:00:00',61),
+	 (21,TO_CLOB('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ReferenciaDocumento>
+    <Identificador>
+        <eEMGDE.Firma.FormatoFirma.ValorCSV>csv01101321321za</eEMGDE.Firma.FormatoFirma.ValorCSV>
+    </Identificador>
+    <Permiso>
+        <Publico>PUBLICO</Publico>
+    </Permiso>
+    <Hash>01e5a46e553015cb3daeadff5a49129c73ee4b60abf9b349bc85b08138942b8c</Hash>
+    <Direccion>http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWsService/CSVQueryDocumentWs?wsdl</Direccion>
+    <Emisor>
+        <Organismo>emisor</Organismo>
+    </Emisor>
+    <Receptor>
+        <Organismo>receptor</Organismo>
+    </Receptor>
+</ReferenciaDocumento>
+'),TIMESTAMP'2023-12-12 00:00:00',21),
+	 (1,TO_CLOB('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ReferenciaDocumento>
+    <Identificador>
+        <eEMGDE.Firma.FormatoFirma.ValorCSV>mixsc</eEMGDE.Firma.FormatoFirma.ValorCSV>
+    </Identificador>
+    <Permiso>
+        <Publico>PUBLICO</Publico>
+    </Permiso>
+    <Hash>01e5a46e553015cb3daeadff5a49129c73ee4b60abf9b349bc85b08138942b8c</Hash>
+    <Direccion>http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWsService/CSVQueryDocumentWs?wsdl</Direccion>
+    <Emisor>
+        <Organismo>emisor</Organismo>
+    </Emisor>
+    <Receptor>
+        <Organismo>receptor</Organismo>
+    </Receptor>
+</ReferenciaDocumento>
+'),TIMESTAMP'2023-11-21 00:00:00',1),
+	 (41,TO_CLOB('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ReferenciaDocumento>
+    <Identificador>
+        <eEMGDE.Firma.FormatoFirma.ValorCSV>csvid</eEMGDE.Firma.FormatoFirma.ValorCSV>
+    </Identificador>
+    <Permiso>
+        <Publico>PUBLICO</Publico>
+    </Permiso>
+    <Hash>a1ddeba635eb8e26cff0ff29d6428c533bbfe57e13be18b04f892e8177c15418</Hash>
+    <Direccion>http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs?wsdl</Direccion>
+    <Emisor>
+        <Organismo>emisor</Organismo>
+    </Emisor>
+    <Receptor>
+        <Organismo>receptor</Organismo>
+    </Receptor>
+</ReferenciaDocumento>
+'),TIMESTAMP'2024-01-18 00:00:00',41),
+	 (42,TO_CLOB('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ReferenciaDocumento>
+    <Identificador>
+        <eEMGDE.Firma.FormatoFirma.ValorCSV>estoeselcsv</eEMGDE.Firma.FormatoFirma.ValorCSV>
+    </Identificador>
+    <Permiso>
+        <Publico>PUBLICO</Publico>
+    </Permiso>
+    <Hash>a1ddeba635eb8e26cff0ff29d6428c533bbfe57e13be18b04f892e8177c15418</Hash>
+    <Direccion>http://localhost:8280//interdocapi/interna/protected/CSVQueryDocumentWs?wsdl</Direccion>
+    <Emisor>
+        <Organismo>emisor</Organismo>
+    </Emisor>
+    <Receptor>
+        <Organismo>receptor</Organismo>
+    </Receptor>
+</ReferenciaDocumento>
+'),TIMESTAMP'2024-01-18 00:00:00',42);
