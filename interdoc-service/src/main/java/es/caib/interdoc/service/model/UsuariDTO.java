8b61dd83cee0511b3ce11e493a10b88f61b32f9d
fbosch-fundaciobit <fbosch@fundaciobit.org>
2025-09-29 18:43:13 +0200

    Crear pantalla de llistat d'usuaris #22

--- interdoc-service/src/main/java/es/caib/interdoc/service/model/UsuariDTO.java
+++ interdoc-service/src/main/java/es/caib/interdoc/service/model/UsuariDTO.java
@@ -15,109 +15,157 @@ import java.time.LocalDate;
  */
 @Schema(name = "Usuari")
 public class UsuariDTO {
-    
-	private Long usuariId;
-	
+
+    @NotNull
+    private Long usuariId;
+
     @NotEmpty
     @Size(max = 255)
     private String username;
-    
-	@NotEmpty
+
+    @NotEmpty
     @Size(max = 5)
     private String idiomaId;
-	
+
     private Long darreraEntitat;
 
-	@NotEmpty
-	@Size(max = 255)
-	private String nom;
-	
-	@NotEmpty
+    @NotEmpty
+    @Size(max = 255)
+    private String nom;
+
+    @NotEmpty
     @Size(max = 255)
     private String llinatge1;
-	
-	@NotEmpty
+
     @Size(max = 255)
     private String llinatge2;
-	
-	@NotEmpty
+
+    @NotEmpty
     @Size(max = 255)
     private String email;
 
-	@NotEmpty
+    @NotEmpty
     @Size(max = 255)
     private String nif;
 
-	
-	
+    public Long getUsuariId() {
+        return usuariId;
+    }
 
-	public Long getUsuariId() {
-		return usuariId;
-	}
-	
-	public String getUsername() {
-	    return username;
-	}
-	
-	public String getIdiomaId() {
-	    return idiomaId;
-	}
-	
-	public Long getDarreraEntitat() {
-	    return darreraEntitat;
-	}
-	
-	public String getNom() {
+    public String getUsername() {
+        return username;
+    }
+
+    public String getIdiomaId() {
+        return idiomaId;
+    }
+
+    public Long getDarreraEntitat() {
+        return darreraEntitat;
+    }
+
+    public String getNom() {
         return nom;
     }
-	
-	public String getLlinatge1() {
+
+    public String getLlinatge1() {
         return llinatge1;
     }
-	
-	public String getLlinatge2() {
+
+    public String getLlinatge2() {
         return llinatge2;
     }
-	
-	public String getEmail() {
-	    return email;
-	}
 
-	public String getNif() {
-		return nif;
-	}
-	
-	public void setUsuariId(Long usuariId) {
-	    this.usuariId = usuariId;
-	}
-	
-	public void setUsername(@NotEmpty @Size(max = 255) String username) {
-	    this.username = username;
-	}
-	
-	public void setIdiomaId(@NotEmpty @Size(max = 5) String idiomaId) {
-	    this.idiomaId = idiomaId;
-	}
-	
-	public void setDarreraEntitat(Long darreraEntitat) {
-	    this.darreraEntitat = darreraEntitat;
-	}
-	
-	public void setNom(@NotEmpty @Size(max = 255) String nom) {
-	    this.nom=nom;
-	}
-	
-	public void setLlinatge1(@NotEmpty @Size(max = 255) String llinatge1) {
-        this.llinatge1=llinatge1;
+    public String getEmail() {
+        return email;
     }
-	
-	public void setLlinatge2(@NotEmpty @Size(max = 255) String llinatge2) {
-        this.llinatge2=llinatge2;
+
+    public String getNif() {
+        return nif;
     }
-	public void setEmail(@NotEmpty @Size(max = 255) String email) {
-        this.email=email;
+
+    public void setUsuariId(Long usuariId) {
+        this.usuariId = usuariId;
     }
-	public void setNif(@NotEmpty @Size(max = 255) String nif) {
-	    this.nif=nif;
-	}
+
+    public void setUsername(@NotEmpty @Size(max = 255)
+    String username) {
+        this.username = username;
+    }
+
+    public void setIdiomaId(@NotEmpty @Size(max = 5)
+    String idiomaId) {
+        this.idiomaId = idiomaId;
+    }
+
+    public void setDarreraEntitat(Long darreraEntitat) {
+        this.darreraEntitat = darreraEntitat;
+    }
+
+    public void setNom(@NotEmpty @Size(max = 255)
+    String nom) {
+        this.nom = nom;
+    }
+
+    public void setLlinatge1(@NotEmpty @Size(max = 255)
+    String llinatge1) {
+        this.llinatge1 = llinatge1;
+    }
+
+    public void setLlinatge2(@NotEmpty @Size(max = 255)
+    String llinatge2) {
+        this.llinatge2 = llinatge2;
+    }
+
+    public void setEmail(@NotEmpty @Size(max = 255)
+    String email) {
+        this.email = email;
+    }
+
+    public void setNif(@NotEmpty @Size(max = 255)
+    String nif) {
+        this.nif = nif;
+    }
+
+    public UsuariDTO() {
+    }
+    
+    public UsuariDTO(Long usuariId,
+            String username,
+            String nom, 
+            String llinatge1,
+            String llinatge2,
+            String email,
+            String nif
+            ) {
+                super();
+                this.usuariId = usuariId;
+                this.username = username;
+                this.nom = nom;
+                this.llinatge1 = llinatge1;
+                this.llinatge2 = llinatge2;
+                this.email = email;
+                this.nif = nif;
+    }
+
+    public UsuariDTO(@NotNull
+    Long usuariId, @NotEmpty @Size(max = 255)
+    String username,
+    Long darreraEntitat,
+    @NotEmpty @Size(max = 5)    String idiomaId, @NotEmpty @Size(max = 255)
+    String nom, @NotEmpty @Size(max = 255)
+    String llinatge1, String llinatge2, @Size(max = 255)
+    String email, 
+    @NotEmpty @Size(max = 255) String nif) {
+        super();
+        this.usuariId = usuariId;
+        this.username = username;
+        this.idiomaId = idiomaId;
+        this.nom = nom;
+        this.llinatge1 = llinatge1;
+        this.llinatge2 = llinatge2;
+        this.email = email;
+        this.nif = nif;
+    }
+
 }
